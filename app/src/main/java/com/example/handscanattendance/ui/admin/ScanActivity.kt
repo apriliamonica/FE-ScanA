aaf75efc1373e4908dab793042e4ca8b0206adc3
April <liapandeiroth34190@gmail.com>
2025-04-15 15:56:37 +0800

    mata kuliah xml

--- app/src/main/java/com/example/handscanattendance/ui/admin/ScanActivity.kt
+++ app/src/main/java/com/example/handscanattendance/ui/admin/ScanActivity.kt
@@ -1,4 +1,12 @@
 package com.example.handscanattendance.ui.admin
 
-class ScanActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.handscanattendance.R
+
+class ScanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan)
+    }
 }
